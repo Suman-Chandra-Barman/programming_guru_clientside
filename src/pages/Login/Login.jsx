de0d93fb059669ae45462db80eadcaf2c35022ea
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-10-25 02:36:06 +0600

    Registration and login form created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,91 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div>
-      <h3>Thi is Login page</h3>
+    <div className="bg-gray-50 py-10">
+      <div className="flex flex-col  items-center min-h-screen pt-6 sm:justify-center sm:pt-0">
+        <div>
+          <h3 className="text-4xl font-bold text-purple-600">
+            Login Your Account
+          </h3>
+        </div>
+        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
+          <form>
+            <div className="mt-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Email
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="email"
+                  name="email"
+                  className="block p-2 border border-1 w-full mt-1 border-gray-300 rounded-md shadow-sm"
+                  placeholder="Enter email"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Password
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="password"
+                  name="password"
+                  className="block p-2 border border-1 w-full mt-1 border-gray-300 rounded-md shadow-sm"
+                  placeholder="Enter password"
+                />
+              </div>
+            </div>
+            <Link to="#" className="text-xs text-purple-600 hover:underline">
+              Forget Password?
+            </Link>
+            <div className="flex items-center mt-4">
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                Login
+              </button>
+            </div>
+          </form>
+          <div className="mt-4 text-grey-600">
+            Don't have an account?{" "}
+            <span>
+              <Link className="text-purple-600 hover:underline" to="/register">
+                Register
+              </Link>
+            </span>
+          </div>
+          <div className="flex items-center w-full my-4">
+            <hr className="w-full" />
+            <p className="px-3 ">OR</p>
+            <hr className="w-full" />
+          </div>
+          <div className="my-6 space-y-2">
+            <button
+              aria-label="Login with Google"
+              type="button"
+              className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
+            >
+              <FaGoogle className="text-2xl" />
+              <p>Login with Google</p>
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
+            >
+              <FaGithub className="text-2xl" />
+              <p>Login with GitHub</p>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
